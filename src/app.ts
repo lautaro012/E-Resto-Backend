d570eb2ca45833ea953d823e91ee1d76f7b23b83
Robles Lautaro <lautaro0121@gmail.com>
2023-03-25 01:16:30 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'));
 app.use(cors())
+app.use((req:any, res:any, next:any) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/',router)
