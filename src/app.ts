bcb42b54069f4aa232fcad69a66f6abbb0643a19
Robles Lautaro <lautaro0121@gmail.com>
2023-03-25 01:19:37 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,16 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'));
-app.use(cors())
 app.use((req:any, res:any, next:any) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 });
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 app.use('/',router)
 
 export default app
